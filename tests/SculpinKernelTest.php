fa0b1409dd647c0b26b2bbc81f2750a57cdc080a
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-07-16 23:18:35 +0200

    Fixed TestCase class name

--- tests/SculpinKernelTest.php
+++ tests/SculpinKernelTest.php
@@ -1,6 +1,8 @@
 <?php
 
-class SculpinKernelTest extends PHPUnit_Framework_TestCase
+use PHPUnit\Framework\TestCase;
+
+class SculpinKernelTest extends TestCase
 {
     public function testGetAdditionalSculpinBundles()
     {
